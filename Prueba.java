8830adac0624981f07558c85ceabfe287b1238cd
Nerea Moreno <nmorferb@g.educaand.es>
2025-11-04 09:54:16 +0100

    Primer commit del proyecto java

---
+++ Prueba.java
@@ -0,0 +1,9 @@
+public class Prueba {
+    public static void main(String[] args) {
+        HolaMundo();
+    }
+
+    public static void HolaMundo(){
+        System.out.println("Hola Mundo");
+    }
+}
